4f30f5e2c834f94d1148f80814a188035c3f2c3b
Nathan-Guenault <nathan.guenault@gmail.com>
2026-04-30 09:01:52 +0200

    Rajout des pages Ontologie et Trous

---
+++ src/components/ProsOrCons.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface ProsOrConsProps{
+    liste: Array<String>;
+}
+
+function ProsOrCons({liste} : ProsOrConsProps){
+  const ligne = []
+  for(let i=0; i<liste.length; i++){
+    ligne.push(<li className="pros-cons">• liste[i]</li>)
+  }
+  
+  return (
+    <ul className="list-pros-cons">
+      {ligne}
+    </ul>
+  )
+}
+
+export default ProsOrCons
